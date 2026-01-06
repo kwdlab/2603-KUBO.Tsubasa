62606cb2452f41118d47717b42f6175a65e123ab
klstu04 <klstu04@m.kwdlab.org>
2026-01-06 20:56:17 +0900

    Add files via upload

---
+++ KeyGenerationProfile.cs
@@ -0,0 +1,34 @@
+﻿using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace MLKemKeyGeneration
+{
+    class Program
+    {
+        static readonly MLKemParameters KemParam =
+            MLKemParameters.ml_kem_512;
+        // MLKemParameters.ml_kem_768;
+        // MLKemParameters.ml_kem_1024;
+
+        static void Main(string[] args)
+        {
+            const int LoopCount = 100000;
+
+            var random = new SecureRandom();
+
+            var kpg = new MLKemKeyPairGenerator();
+            kpg.Init(new MLKemKeyGenerationParameters(
+                random,
+                KemParam
+            ));
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var kp = kpg.GenerateKeyPair();
+            }
+
+            Console.WriteLine($"ML-KEM Key Generation finished: {KemParam.Name}");
+        }
+    }
+}

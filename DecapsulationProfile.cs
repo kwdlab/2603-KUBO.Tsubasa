8016410a728a364f1afbc12f123c9b8c95a01ea7
klstu04 <klstu04@m.kwdlab.org>
2026-01-06 21:46:23 +0900

    Add files via upload

---
+++ DecapsulationProfile.cs
@@ -0,0 +1,49 @@
+﻿using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace MLKemDecapsulation
+{
+    class DecapsulationProfile
+    {
+        static void Main(string[] args)
+        {
+            var kemParam = MLKemParameters.ml_kem_512;
+            var kemName = "ML-KEM-512";
+
+            const int LoopCount = 100000;
+
+            var random = new SecureRandom();
+
+            var kpg = new MLKemKeyPairGenerator();
+            kpg.Init(new MLKemKeyGenerationParameters(random, kemParam));
+            var kp = kpg.GenerateKeyPair();
+
+            var encapsulator = KemUtilities.GetEncapsulator(kemName);
+            encapsulator.Init(kp.Public);
+
+            byte[] ciphertext = new byte[encapsulator.EncapsulationLength];
+            byte[] encapSecret = new byte[encapsulator.SecretLength];
+
+            encapsulator.Encapsulate(
+                ciphertext, 0, ciphertext.Length,
+                encapSecret, 0, encapSecret.Length
+            );
+
+            var decapsulator = KemUtilities.GetDecapsulator(kemName);
+            decapsulator.Init(kp.Private);
+
+            byte[] decapSecret = new byte[decapsulator.SecretLength];
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                decapsulator.Decapsulate(
+                    ciphertext, 0, ciphertext.Length,
+                    decapSecret, 0, decapSecret.Length
+                );
+            }
+
+            Console.WriteLine($"ML-KEM Decapsulation finished: {kemParam.Name}");
+        }
+    }
+}
